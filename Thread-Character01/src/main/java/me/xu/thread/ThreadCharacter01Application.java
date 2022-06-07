fcd1d9bc130ba878e462d4de8b0b735c0946f987
文享旭 <846212939@qq.com>
2022-06-07 16:13:20 +0800

    Initial commit

---
+++ Thread-Character01/src/main/java/me/xu/thread/ThreadCharacter01Application.java
@@ -0,0 +1,13 @@
+package me.xu.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadCharacter01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadCharacter01Application.class, args);
+    }
+
+}
---
+++ Thread-Character01/src/test/java/me/xu/thread/ThreadCharacter01ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xu.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadCharacter01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
