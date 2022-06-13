fb93cf6839c07897fedb8b69ec9073abd92b58d5
文享旭 <846212939@qq.com>
2022-06-13 11:11:27 +0800

    feat:java多线程-启动线程初始化

---
+++ Thread-Character02/src/test/java/me/xu/thread/ThreadCharacter02ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xu.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadCharacter02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
