6b6d94a4f8b773ec587f2e61569525dff77a64c3
文享旭 <846212939@qq.com>
2022-06-14 13:42:22 +0800

    feat:第三章案例初始化

---
+++ Thread-Character03/src/test/java/me/xu/thread/ThreadCharacter03ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xu.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadCharacter03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
